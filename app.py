63c5fc0c9fcb2335509a90ff3d63e7386c237975
Joshua Walker <jdwalker4598@gmail.com>
2025-09-20 23:53:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
+import roboflow
+rf = roboflow.Roboflow(api_key='wiFvnwMMGiFQxyQzTFN3')
+project = rf.workspace().project('parking-lot-project-akaxh')
+version = project.version(2)
+model = version.model
 
+result = model.predict("parkingLotDelete.jpg", confidence=95, overlap=4).json()
